e699fda41df115d62cfedfae8b5a837313dec8fc
1007283085q <1007283085@qq.com>
2021-09-27 17:21:42 +0800

    初始化自己的学习项目

---
+++ code/src/main/java/com/begin/code/CodeApplication.java
@@ -0,0 +1,18 @@
+package com.begin.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(CodeApplication.class, args);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
