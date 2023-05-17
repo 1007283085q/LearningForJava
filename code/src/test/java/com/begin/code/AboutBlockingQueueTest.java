522e35ea329184cd80fb34babfeb606899415637
0027021570 <quan.jiaxing2@iwhalecloud.com>
2023-05-17 15:08:57 +0800

    关于无界队列的消费问题

--- code/src/test/java/com/begin/code/AboutBlockingQueueTest.java
+++ code/src/test/java/com/begin/code/AboutBlockingQueueTest.java
@@ -2,8 +2,14 @@ package com.begin.code;
 
 import org.json.JSONObject;
 
+import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @ClassName AboutBlockingQueueTest
@@ -28,7 +34,9 @@ public class AboutBlockingQueueTest {
                 while (true) {
                     queue.put(produce());
                     taskId++;
-                    Thread.sleep(1000);
+                    Random random = new Random();
+                    int randomNumber = random.nextInt(1501) + 500;
+                    Thread.sleep(randomNumber);
                 }
             }
             catch (Exception ex) {
@@ -74,13 +82,54 @@ public class AboutBlockingQueueTest {
         Consumer c1 = new Consumer(q);
         Consumer c2 = new Consumer(q);
 //        Consumer c3 = new Consumer(q);
-        new Thread(p).start();
-        new Thread(c1, "当前是threadC1执行消费").start();
-        new Thread(c2, "当前是threadC2执行消费").start();
+//        new Thread(p).start();
+//        new Thread(c1, "当前是threadC1执行消费").start();
+//        new Thread(c2, "当前是threadC2执行消费").start();
 //        new Thread(c3, "当前是threadC3执行消费").start();
         //为什么会是两个线程交替执行呢，唤醒问题
         //如果消费速度比生产速度慢，新的线程会自动争夺后续的新插入任务，猜测有唤醒所有线程的方法
+        ExecutorService executor =
+                new ThreadPoolExecutor(3, 3, 0L, TimeUnit.MILLISECONDS, q,
+                       new NamedThreadFactory());
 
+
+        while (true){
+            executor.execute(() -> {
+                Thread thread = Thread.currentThread();
+                System.out.println(thread.getName() + "|开始消费|");
+                try {
+                    Thread.sleep(5000);
+                }
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+    }
+    private static class NamedThreadFactory implements ThreadFactory {
+        private final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        public NamedThreadFactory() {
+            namePrefix = "mypool-" + poolNumber.getAndIncrement() + "-thread-";
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r, namePrefix + threadNumber.getAndIncrement());
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
+
+        public void threadDestroyed() {
+            threadNumber.decrementAndGet();
+        }
     }
 
 }
